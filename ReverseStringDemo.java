4b309b0d7b93209b5cf4adc2941957a6d19537d9
priyajadhavwebapp <pj0953532@gmail.com>
2026-04-19 18:57:40 +0530

    Add files via upload

---
+++ ReverseStringDemo.java
@@ -0,0 +1,30 @@
+package com.string.reverse;
+
+import java.util.*;
+public class ReverseStringDemo {
+
+    // User-defined function
+    public static String reverseString(String str) {
+        String reversed = "";
+
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reversed += str.charAt(i);
+        }
+
+        return reversed;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String input = sc.nextLine();
+
+        String result = reverseString(input);
+
+        System.out.println("Reversed String: " + result);
+
+        sc.close();
+    }
+}
