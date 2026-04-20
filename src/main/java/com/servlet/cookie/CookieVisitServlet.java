58944ed1cb0d1016e6a958856fbb77df7705c765
priyajadhavwebapp <pj0953532@gmail.com>
2026-04-20 19:36:58 +0530

    commit

---
+++ src/main/java/com/servlet/cookie/CookieVisitServlet.java
@@ -0,0 +1,54 @@
+package com.servlet.cookie;
+
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+public class CookieVisitServlet extends HttpServlet {
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+
+        String name = request.getParameter("username");
+        int visitCount = 1;
+
+        Cookie[] cookies = request.getCookies();
+
+        // Check existing cookies
+        if (cookies != null) {
+            for (Cookie c : cookies) {
+
+                if (c.getName().equals("visitCount")) {
+                    visitCount = Integer.parseInt(c.getValue());
+                    visitCount++;
+                }
+
+                if (c.getName().equals("username")) {
+                    name = c.getValue();
+                }
+            }
+        }
+
+        // Create cookies
+        Cookie nameCookie = new Cookie("username", name);
+        Cookie visitCookie = new Cookie("visitCount", String.valueOf(visitCount));
+
+        // Set expiry (1 minute)
+        nameCookie.setMaxAge(60);
+        visitCookie.setMaxAge(60);
+
+        // Add cookies
+        response.addCookie(nameCookie);
+        response.addCookie(visitCookie);
+
+        // Output
+        out.println("<html><body>");
+        out.println("<h2>Welcome back " + name + "!</h2>");
+        out.println("<p>You have visited this page " + visitCount + " times.</p>");
+        out.println("<p><b>Note:</b> Cookie expires after 60 seconds.</p>");
+        out.println("</body></html>");
+    }
+}
